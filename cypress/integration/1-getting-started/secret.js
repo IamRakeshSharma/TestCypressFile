2b9140b7a81008887dd0136afdbeb5177dc6ce30
Rakesh Basavaraju <rakeshsb@gmail.com>
2022-03-11 22:27:55 -0500

    test

---
+++ cypress/integration/1-getting-started/secret.js
@@ -0,0 +1,27 @@
+import AES from 'crypto-js/aes.js';
+import Utf8 from 'crypto-js/enc-utf8.js';
+
+const encryptWithAES = (text, passphrase) => {
+  return AES.encrypt(text, passphrase).toString();
+};
+
+const decryptWithAES = (ciphertext, passphrase) => {
+  const bytes = AES.decrypt(ciphertext, passphrase);
+  const originalText = bytes.toString(Utf8);
+  return originalText;
+};
+
+global.testAES = () => {
+  const inputText = 'Hello World';
+  const passphrase = 'My Secret Passphrase';
+
+  Logger.log({ inputText });
+
+  const encryptedText = encryptWithAES(inputText, passphrase);
+  Logger.log({ encryptedText });
+
+  const decryptedText = decryptWithAES(encryptedText, passphrase);
+  Logger.log({ decryptedText });
+  console.log({ decryptedText });
+  console.log("Hello world!");
+};
